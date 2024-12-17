fdd357eb390fd0b85bcb7e6d06b44b8a512028a4
fabiansichert <fabian.sichert.1@gmx.de>
2024-12-17 08:24:09 +0100

    final state

--- banking/__init__.py
+++ banking/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://fabian:pass@127.0.0.1:3307/bankingdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "fhasidhas"
+app.config['SECRET_KEY'] = "Passwort123"
 
 db = SQLAlchemy(app)
 
--- banking/routes.py
+++ banking/routes.py
@@ -1,116 +1,129 @@
 from banking import app, db
+import pyotp
+import qrcode
 from flask import render_template, request, redirect, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from sqlalchemy import text
+
+# NEU - Bruteforce Limit
+limiter = Limiter(get_remote_address, app=app)
+
+# NEU - WTF Forms nutzen
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
+
+class TwoFactorForm(FlaskForm):
+    otp_code = StringField('Enter 2FA Code', validators=[DataRequired()])
+    submit = SubmitField('Verify')
 
 @app.route("/")
 def start():
     return redirect(url_for('login'))
 
-@app.route("/logout")
-def logout():
-    session.pop('user_id', None)
-    session.pop('username',None)
-    return redirect(url_for('login'))
-
-@app.route("/login", methods=["GET","POST"])
-def login():
-    if request.method == 'POST':
-        username = request.form.get('Username')
-        password = request.form.get('Password')
-
-        if (username is None or isinstance(username, str) is False):
-            print("something wrong with username")
-            return render_template('login.html')
-        if (password is None or isinstance(username, str) is False):
-            print("something wrong with password")
-            return render_template('login.html')
-
-        query_stmt = f"select id from users where username='{username}' and passwd='{password}';"
-        print(query_stmt)
-        result = db.session.execute(text(query_stmt))
-        user = result.fetchall()
-
-        if not user:
-            print("Try again ...")
-            return render_template("login.html")
-        
-        print(user[0][0])
-        session['user_id'] = user[0][0]
-        session['username'] = username
-        
-        return redirect(url_for('home'))
-    return render_template('login.html')
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        print(request.form)
-        username = request.form.get('Username')
-        password = request.form.get('Password')
-        confirmpassword = request.form.get('ConfirmPassword')
+    form = RegisterForm()
 
-        print(username)
-        print(password)
-        print(confirmpassword)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
+        confirmpassword = form.confirm_password.data
 
-        if (username is None or isinstance(username, str) is False):
-            print("something wrong with username")
-            return render_template('register.html')
-        if (password is None or isinstance(username, str) is False):
-            print("something wrong with password")
-            return render_template('register.html')
-        if (password != confirmpassword):
-            print("something wrong with confirmpassword")
-            return render_template('register.html')
-
-        query_stmt = f"select username from users"
+        query_stmt = f"SELECT username FROM users WHERE username='{username}'"
         result = db.session.execute(text(query_stmt))
         usernames = result.fetchall()
 
-        if not usernames:
-            print("Try again ...")
-            return render_template("register.html")
+        if usernames:
+            return render_template('register.html', form=form, error="Username already exists!")
 
-        if username in usernames:
-            print("Username already exists!")
-            return render_template('register.html')
-        
-        query_stmt = f"""
-        INSERT INTO users (username, passwd, amount) VALUES ('{username}', '{password}', '0.00');
-        """
-        db.session.execute(text(query_stmt))
+        if password != confirmpassword:
+            return render_template('register.html', form=form, error="Passwords do not match!")
+
+        totp = pyotp.random_base32()
+
+        db.session.execute(text("INSERT INTO users (username, passwd, otp_secret, amount) VALUES (:username, :passwd, :otp_secret, '0.00')"),
+                           {"username": username, "passwd": password, "otp_secret": totp})
         db.session.commit()
 
-        query_stmt = f"select id from users where username='{username}'"
+        otp_uri = pyotp.totp.TOTP(totp).provisioning_uri(username, issuer_name="MyApp")
+
+        img = qrcode.make(otp_uri)
+        img.save("banking/static/qrcode.png")
+
+        return render_template('register.html', form=form, qr_code="/static/qrcode.png")
+
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+@limiter.limit("3 per minute")  # NEU! 3 Versuche pro Minute
+def login():
+    form = LoginForm()
+
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
+
+        query_stmt = f"SELECT id, passwd, otp_secret FROM users WHERE username='{username}';"
         result = db.session.execute(text(query_stmt))
         user = result.fetchall()
 
+        if not user or user[0][1] != password:
+            return render_template("login.html", form=form, error="incorrect")
+
         session['user_id'] = user[0][0]
         session['username'] = username
-        
-        return redirect(url_for('home'))
-    return render_template("register.html")
+        session['otp_secret'] = user[0][2]
+
+        return redirect(url_for('two_factor'))
+
+    return render_template('login.html', form=form)
+
+#NEU - 2FA
+@app.route("/two_factor", methods=["GET", "POST"])
+def two_factor():
+    if request.method == 'POST':
+        otp_code = request.form.get('totp_code')
+        totp = pyotp.TOTP(session['otp_secret'])
+
+        if totp.verify(otp_code):
+            session['authenticated'] = True
+            return redirect(url_for('home'))
+        else:
+            return render_template('two_factor.html', error="Invalid 2FA code.")
+
+    return render_template('two_factor.html')
+
 
 @app.route("/home")
 def home():
-    if "user_id" not in session:
-        return render_template("login.html")
-    
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+
     userid = session["user_id"]
     username = session['username']
 
-    query_stmt = f"select * from users where id={userid}"
-    print(query_stmt)
+    query_stmt = f"SELECT * FROM users WHERE id={userid}"
     result = db.session.execute(text(query_stmt))
     usersquery = result.fetchall()
 
-    query_stmt = f"""
+    query_stmt = """
         SELECT t.id, t.from_user, t.to_user, t.amount, t.transaction_date, 
                u_from.username AS from_username, u_to.username AS to_username 
         FROM transactions t
         JOIN users u_from ON t.from_user = u_from.id
         JOIN users u_to ON t.to_user = u_to.id
-        WHERE t.from_user = {userid} OR t.to_user = {userid}
+        WHERE t.from_user = :userid OR t.to_user = :userid
     """
     result = db.session.execute(text(query_stmt), {'userid': userid})
     transactionquery = result.fetchall()
@@ -130,15 +143,22 @@ def home():
             "date": transaction.transaction_date.strftime("%d.%m.%Y")
         })
 
-    return render_template("home.html",username=username, person=usersquery, transactions=transactions)
+    return render_template("home.html", username=username, person=usersquery, transactions=transactions)
+
+@app.route("/logout")
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    session.pop('otp_secret', None)
+    session.pop('authenticated', None)
+    return redirect(url_for('login'))
 
 @app.route("/overview")
 def overview():
-
     search_query = request.args.get('search', '')
-    
+
     if search_query:
-        query_stmt = f"SELECT * FROM users WHERE username LIKE '%{search_query}%'"
+        query_stmt = "SELECT * FROM users WHERE username LIKE '%:search_query%'"
         result = db.session.execute(text(query_stmt))
     else:
         query_stmt = "SELECT * FROM users"
